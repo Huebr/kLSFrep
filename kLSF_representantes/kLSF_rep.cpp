15de8cce2213663d6df8153d1d83599168419329
HueBr <pedro.jorge@aluno.uece.br>
2019-01-15 08:36:00 -0300

    quase la

--- kLSF_representantes/kLSF_rep.cpp
+++ kLSF_representantes/kLSF_rep.cpp
@@ -2,15 +2,23 @@
 #include <utility>                   // for std::pair
 #include <algorithm>                 // for std::for_each
 #include <boost/graph/graph_traits.hpp> // for creation of descriptors vertex and edges.
-#include <boost/graph/adjacency_list.hpp> //for usage of adjacency list]
-#include <boost/graph/filtered_graph.hpp>
+#include <boost/graph/adjacency_list.hpp> //for usage of adjacency list
+#include <boost/graph/graphml.hpp>
 #include <boost/graph/connected_components.hpp>
 #include <ilcplex/ilocplex.h>
 #include <boost/dynamic_bitset.hpp>
-#include <unordered_set>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/graph/filtered_graph.hpp>
+#include <boost/exception/all.hpp>
+#include <exception>
+#include <vector>
 ILOSTLBEGIN //initialization make vs work properly
 
 using namespace boost;
+namespace po = boost::program_options;
 typedef adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> graph_t;
 //peharps later remove supersource
 typedef dynamic_bitset<> db;
@@ -27,8 +35,15 @@ void print_edges(EdgeIter first, EdgeIter last, const Graph& G) {
 		std::cout << "Edge: " << "(" << source(*it, G) << "," << target(*it, G) << ") " << " Color: " << colors[*it] << "\n";
 		std::cout << "Edge: " << "(" << target(*it, G) << "," << source(*it, G) << ") " << " Color: " << colors[*it] << "\n";
 	}
-	cout << " Number of vertex: " << num_vertices(G) << std::endl;
-	cout << " Number of edges: " << num_edges(G) << std::endl;
+	std::cout << " Number of vertex: " << num_vertices(G) << std::endl;
+	std::cout << " Number of edges: " << num_edges(G) << std::endl;
+	std::vector<int> components(num_vertices(G));
+	int num = connected_components(G, &components[0]);
+	std::vector<int>::size_type i;
+	std::cout << "Total number of components: " << num << std::endl;
+	for (i = 0; i != components.size(); ++i)
+		std::cout << "Vertex " << i << " is in component " << components[i] << std::endl;
+	std::cout << std::endl;
 }
 
 template <typename EdgeColorMap, typename ValidColorsMap>
@@ -79,8 +94,7 @@ property_map<graph_t, edge_color_t>::type get_colors(Graph &g) {
 //basic definitions
 typedef IloArray<IloBoolVarArray> IloVarMatrix;
 
-//cuts 
-ILOLAZYCONSTRAINTCALLBACK4(MyLazyCall,IloBoolVarArray,Z,IloBoolVarArray,Y, IloVarMatrix,X ,graph_t,g ) {
+/*ILOUSERCUTCALLBACK5(MyUserCall, IloBoolVarArray, Z, IloBoolVarArray, Y, IloVarMatrix, X, int, k, graph_t, g) {
 	int size = Z.getSize();
 	int n_vertices = Y.getSize();
 	db temp(size);
@@ -88,43 +102,113 @@ ILOLAZYCONSTRAINTCALLBACK4(MyLazyCall,IloBoolVarArray,Z,IloBoolVarArray,Y, IloVa
 	std::vector<int> components(num_vertices(g));
 	auto colors = get_colors(g);
 	graph_traits<graph_t>::edge_iterator it, end;
-	for (int j = 1; j < n_vertices; ++j) { //using colors of original graph
-		if (getValue(Y[j])==1) n_comp_sol+=1;
+	for (int i = 0; i < n_vertices; ++i) { //using colors of original graph
+		if (getValue(Y[i]) == 1) n_comp_sol++;
 	}
 	for (int i = 0; i < size; ++i) { //using colors of original graph
-		if (getValue(Z[i])==1) temp.set(i);
+		if (getValue(Z[i]) == 1) temp.set(i);
 	}
-	std::cout << " user cutting" << std::endl;
-	int num_c = get_components(g, temp, components) - 1;//vertex 0 isolated
-	if (num_c !=n_comp_sol) {
-		std::cout << "add user cut" << std::endl;
-		for (volatile int u = 1; u < n_vertices; ++u) {
+	//std::cout << " user cutting" << std::endl;
+	int num_c = get_components(g, temp, components);//vertex 0 isolated
+	//std::cout << "add user cut: " << std::endl;
+	if (num_c != n_comp_sol) {
+		for (int u = 0; u < n_vertices; ++u) {
 			if (getValue(Y[u]) == 1) {
-				for (volatile int v = u + 1; v < n_vertices; ++v) {
+				for (int v = u + 1; v < n_vertices; ++v) {
 					if (getValue(X[u][v]) == 1) {
 						db temp1(size);
+						volatile int s, t;
 						IloExpr expr(getEnv());
+						//IloExpr expr2(getEnv());
 						if (components[u] != components[v]) {
 							std::tie(it, end) = edges(g);
 							while (it != end) {
-								if (components[source(*it, g)] != components[target(*it, g)]) {
-									if (components[source(*it, g)] == components[u] || components[target(*it, g)] == components[u]) {
-										temp1.set(colors[*it]);
-									}
-								}
-								++it;
+							s = source(*it, g);
+							t = target(*it, g);
+							if (components[s] != components[t]) {
+							if (components[s] == components[u] || components[t] == components[u]) {
+							temp1.set(colors[*it]);
 							}
-							for (int i = 0; i < Z.getSize(); ++i) if (temp1.test(i))expr += Z[i];
-							add(expr >= X[u][v]).end();
+							}
+							++it;
+							}
+							for (int i = 0; i < Z.getSize(); ++i) {
+								if (temp.test(i))expr += Z[i];
+								//if (temp1.test(i))expr2 += Z[i];
+							}
+							//std::cout << (expr <= k - X[u][v]) << std::endl;
+							add(expr <= k - X[u][v]).end();
+							//add(expr2 >= X[u][v]).end();
 							expr.end();
+							//expr2.end();
+
 						}
-							
+
 					}
 				}
 			}
 		}
 	}
-	else std::cout << "is good" << std::endl;
+}*/
+//cuts 
+ILOLAZYCONSTRAINTCALLBACK5(MyLazyCall,IloBoolVarArray,Z,IloBoolVarArray,Y, IloVarMatrix,X ,int,k,graph_t,g ) {
+	int size = Z.getSize();
+	int n_vertices = Y.getSize();
+	db temp(size);
+	int n_comp_sol = 0;
+	std::vector<int> components(num_vertices(g));
+	auto colors = get_colors(g);
+	std::cout << "add lazy cut: " << std::endl;
+	graph_traits<graph_t>::edge_iterator it, end;
+	for (int i = 0; i < n_vertices; ++i) { //using colors of original graph
+		std::cout << "Y[" << i << "]= " << getValue(Y[i]) << std::endl;
+		if (getValue(Y[i])==1) n_comp_sol++;
+	}
+	for (int i = 0; i < size; ++i) { //using colors of original graph
+		std::cout <<"Z["<<i<<"]= "<< getValue(Z[i]) << std::endl;
+		if (getValue(Z[i])==1) temp.set(i);
+	}
+	//std::cout << " user cutting" << std::endl;
+	int num_c = get_components(g, temp, components);//vertex 0 isolated
+
+	for (int u = 0; u < n_vertices; ++u) {
+			if (getValue(Y[u])==1) {
+				for (int v = u + 1; v < n_vertices; ++v) {
+					std::cout << "X[" << u <<","<<v<< "]= " << getValue(X[u][v]) << std::endl;
+					if (getValue(X[u][v])>0) {
+						db temp1(size);
+						volatile int s, t;
+						IloExpr expr(getEnv());
+						IloExpr expr2(getEnv());
+						if (components[u] != components[v]) {
+							std::tie(it, end) = edges(g);
+							while (it != end) {
+								s = source(*it, g);
+								t = target(*it, g);
+								if (components[s] != components[t]) {
+								if (components[s] == components[u] || components[t] == components[u]) {
+									temp1.set(colors[*it]);
+								}
+							}
+							++it;
+							}
+							for (int i = 0; i < Z.getSize(); ++i) {
+								if (temp.test(i))expr += Z[i];
+								if (temp1.test(i))expr2 += Z[i];
+							}
+							std::cout << (expr <= k - X[u][v]) << std::endl;
+							std::cout << (expr2 >= X[u][v]) << std::endl;
+							add(expr <= k - X[u][v]).end();
+							add(expr2 >= X[u][v]).end();
+							expr.end();
+							expr2.end();
+
+						}
+
+					}
+				}
+			}
+	}
 }
 
 
@@ -137,11 +221,10 @@ void buildRepModel(IloModel mod, IloBoolVarArray Y, IloBoolVarArray Z, IloVarMat
 	//modelling objective function
 	IloExpr exp(env);
 	int n_vertices = num_vertices(g);
-	for (int i = 1; i < n_vertices; ++i) {
+	for (int i = 0; i < n_vertices; ++i) {
 		exp += Y[i];
 		Y[i].setName(("y" + std::to_string(i)).c_str());
 	}
-	Y[0].setName("y0");
 	mod.add(IloMinimize(env, exp));
 	exp.end();
 
@@ -161,15 +244,15 @@ void buildRepModel(IloModel mod, IloBoolVarArray Y, IloBoolVarArray Z, IloVarMat
 
 
 	//first constraint 
-	for (int u = 1; u < n_vertices; ++u) {
+	for (int u = 0; u < n_vertices; ++u) {
 		for (int v = u + 1; v<n_vertices; ++v) mod.add(X[u][v]<=Y[u]);
 	}
 	//second constraint
 	IloExpr e2(env);
-	for (int u = 1; u < n_vertices; ++u) {
+	for (int u = 0; u < n_vertices; ++u) {
 		for (int v = u + 1; v<n_vertices; ++v) e2 += X[u][v];
 	}
-	for (int u = 1; u < n_vertices; ++u) e2 += Y[u];
+	for (int u = 0; u < n_vertices; ++u) e2 += Y[u];
 	mod.add(e2 == n_vertices);
 	e2.end();
 
@@ -187,8 +270,8 @@ void buildRepModel(IloModel mod, IloBoolVarArray Y, IloBoolVarArray Z, IloVarMat
 		exptreecut += Z[colors[*it]];
 		++it;
 	}
-	int N = num_vertices(g) - 1;
-	for (int u = 1; u < n_vertices; ++u) exptreecut += Y[u];
+	int N = num_vertices(g);
+	for (int u = 0; u < n_vertices; ++u) exptreecut += Y[u];
 	mod.add(exptreecut >= N);
 	exptreecut.end();
 
@@ -222,61 +305,52 @@ void buildRepModel(IloModel mod, IloBoolVarArray Y, IloBoolVarArray Z, IloVarMat
 }
 
 
-
-
-int main()
-{
-	enum { A, B, C, D, E, F, G, H };
-	typedef std::pair<int, int> Edge;
-	graph_traits<graph_t>::edge_iterator it, end;
-	const int n_vertices = 14;
-	const int k = 4;
-	Edge edge_array[] = {
-		Edge(1,2),  Edge(1,12), Edge(2,3),  Edge(3,4),
-		Edge(4,5),  Edge(5,6),  Edge(5,8),  Edge(5,9),
-		Edge(5,11), Edge(5,13), Edge(6,7),  Edge(7,8),
-		Edge(8,9),  Edge(9,10), Edge(10,11),Edge(11,12),
-		Edge(12,13),
-	};
-	int n_edges = sizeof(edge_array) / sizeof(edge_array[0]);
-	const int colors[] = { H,H,D,D,C,F,E,D,C,F,G,E,A,B,G,A,B };
-
-	graph_t g(edge_array, edge_array + n_edges, colors, n_vertices);
-	//add edges to super source vertex index 0. remember!!!
-	std::unordered_set<int> st(colors, colors + sizeof(colors) / sizeof(colors[0]));
-	int n_colors = st.size();
-	st.clear();//note use end because it is an iterator
-	//for (int i = 1; i < n_vertices; ++i) boost::add_edge(0, i, property<edge_color_t, int>(n_colors + i - 1), g);
-	std::tie(it, end) = boost::edges(g);
-	print_edges(it, end, g);
-
-
-	//temporario contar numero de cores
-	//n_colors += n_vertices - 1;
-
-
+template<class Graph>
+void solveModel(int n_vertices, int n_colors, int k, Graph &g) {
 	//starting cplex code part
-	IloEnv   env; //environment
+	IloEnv env; //environment
 	try {
 		IloModel model(env);
 		IloBoolVarArray Y(env, n_vertices), Z(env, n_colors);
-		IloVarMatrix    X(env, n_vertices); 
+		IloVarMatrix    X(env, n_vertices);
 		buildRepModel(model, Y, Z, X, k, g);
 		IloCplex cplex(model);
 		cplex.exportModel("kSLF_rep.lp"); // good to see if the model is correct
-											//cross your fingers
-		cplex.use(MyLazyCall(env,Z,Y,X,g));
+										  //cross your fingers
+		//cplex.setParam(IloCplex::Param::Preprocessing::Presolve, 0);
+		//cplex.setParam(IloCplex::Param::MIP::Display, 5);
+		//cplex.setParam(IloCplex::Param::Tune::Display, 3);
+		//cplex.setParam(IloCplex::Param::Simplex::Display, 2);
+		//cplex.setParam(IloCplex::Param::Preprocessing::Dual, 0);
+		//cplex.setParam(IloCplex::PreInd, 0);
+		//cplex.use(MyUserCall(env, Z, Y, X, k, g));
+		cplex.use(MyLazyCall(env, Z, Y, X,k, g));
 		cplex.solve();
 		cplex.out() << "solution status = " << cplex.getStatus() << endl;
 
 		cplex.out() << endl;
 		cplex.out() << "Number of components   = " << cplex.getObjValue() << endl;
+		db temp(n_colors);
+		cplex.out() << "color(s) solution:";
 		for (int i = 0; i < Z.getSize(); i++) {
-			cplex.out() << "  Z" << i << " = " << cplex.getValue(Z[i]) << endl;
+			if (cplex.getValue(Z[i])) {
+				temp.set(i);
+				cplex.out() << " " << i;
+			}
 		}
-		for (int i = 1; i < Y.getSize(); ++i) {
-			cplex.out() << "  Y" << i << " = " << cplex.getValue(Y[i]) << endl;
+		cplex.out() << endl;
+		cplex.out() << "root(s) solution:";
+		for (int i = 0; i < Y.getSize(); i++) {
+			if (cplex.getValue(Y[i])) {
+				cplex.out() << " " << i;
+				/*for (int j = i + 1; j < n_vertices; j++) {
+					cplex.out() <<std::endl <<"X["<<i<<","<<j<<"]= " << cplex.getValue(X[i][j])<<std::endl;
+				}*/
+			}
 		}
+		cplex.out() << endl;
+		print_filtered_graph(g,temp);
+
 
 	}
 	catch (IloException& e) {
@@ -287,6 +361,94 @@ int main()
 	}
 	//memory cleaning
 	env.end();
+}
+
+
+int main(int argc, const char *argv[])
+{
+	typedef adjacency_list<vecS, vecS, undirectedS, no_property, property<edge_color_t, int>> Graph;
+	typedef std::pair<int, int> Edge;
+	typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
+	Graph::edge_iterator it, end;
+	Graph g;
+	int n_vertices, n_colors;
+	//command-line processor
+
+	try {
+		std::ifstream ifn;
+		po::options_description desc{ "Options" };
+		desc.add_options()("help,h", "produce help message")
+			("input-file,i", po::value< string >(), "input file")
+			("include-path,I", po::value< string >(), "include path")
+			("setup-file", po::value< string >(), "setup file");
+		po::positional_options_description p;
+		p.add("input-file", -1);
+
+
+		po::variables_map vm;
+		po::store(po::command_line_parser(argc, argv).
+			options(desc).positional(p).run(), vm);
+		po::notify(vm);
+
+		if (vm.count("help")) {
+			cout << desc << "\n";
+			return 1;
+		}
+		else if (vm.count("input-file"))
+		{
+			std::cout << "Input files are: " << vm["input-file"].as<string>() << "\n";
+			if (vm.count("include-path"))ifn.open((vm["include-path"].as<string>() + vm["input-file"].as<string>()).c_str(), ifstream::in);
+			else ifn.open(vm["input-file"].as<string>().c_str(), ifstream::in);
+			if (!ifn.is_open()) {
+				std::cout << "error opening file" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+			dynamic_properties dp;
+			dp.property("color", get(edge_color, g));
+			read_graphml(ifn, g, dp);
+
+			vector<string> vecI;
+			split(vecI, vm["input-file"].as<string>(), is_any_of("-."), token_compress_off);
+			if (vecI.size() == 6) {
+				std::cout << vecI[0] << std::endl;
+				n_vertices = stoi(vecI[0]);
+				std::cout << vecI[2] << std::endl;
+				n_colors = stoi(vecI[2]);
+				std::cout << vecI[3] << std::endl;
+				int k = stoi(vecI[3]);
+				//add edges to super source vertex. remember!!!
+				//vertex_t u = add_vertex(g);
+				//n_vertices++;
+				//for (int i = 0; i < n_vertices - 1; ++i) boost::add_edge(u, i, property<edge_color_t, int>(n_colors++), g);
+				//std::tie(it, end) = boost::edges(g);
+				//print_edges(it, end, g);
+				solveModel(n_vertices, n_colors, k, g);
+			}
+			else {
+				std::cout << "file wrong name format." << std::endl;
+			}
+
+		}
+		else if (vm.count("setup-file")) {
+			std::cout << "Not implemented yet" << std::endl;
+		}
+		else {
+			std::cout << "see options(-h)." << std::endl;
+		}
+
+
+	}
+	catch (const po::error &ex) {
+		std::cout << ex.what();
+		exit(EXIT_FAILURE);
+	}
+	catch (boost::exception &ex) {
+		std::cout << boost::diagnostic_information(ex) << std::endl;
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what();
+		exit(EXIT_FAILURE);
+	}
 
 	return 0;
 }
